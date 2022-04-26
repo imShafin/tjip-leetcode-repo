3450bc2c26306ece5cc3a58ab5efc0ab506aac23
imShafin <izarahsha2357@gmail.com>
2022-04-26 09:48:57 +0600

    Added new Solution for week03

---
+++ week03/Isomorphic_Strings.cpp
@@ -0,0 +1,16 @@
+// TC: O(NlogN)
+// MC: O(N)
+class Solution {
+public:
+    bool isIsomorphic(string s, string t, int interval = 0) {
+        unordered_map<char, char> track;
+        for (int i = 0; i < s.size(); i++) {
+            if (track.count(s[i]) == 1 && track[s[i]] != t[i] ) {
+                return false;
+            }
+            track[s[i]] = t[i];
+        }
+        if(interval == 0) return isIsomorphic(t, s, 1);
+        return true;
+    }
+};
