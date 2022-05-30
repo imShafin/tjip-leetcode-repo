308a632df9da368f42cd94c53413fc54144f03e0
imShafin <izarahsha2357@gmail.com>
2022-05-30 23:30:30 +0600

    solution for week05

---
+++ week05/Remove_Nth_Node_From_End_of_List.cpp
@@ -0,0 +1,23 @@
+/*
+TC: O(N) N means number of nodes
+MC: O(1)
+*/
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+      ListNode* forwardStep = head;
+      ListNode* backStep = head;
+      while (n--)
+      {
+        forwardStep = forwardStep -> next;
+      }
+      if (!forwardStep) return head -> next;
+      while(forwardStep -> next)
+      {
+        backStep = backStep -> next;
+        forwardStep = forwardStep -> next;
+      }
+      backStep -> next = backStep -> next -> next;
+      return head;
+    }
+};
