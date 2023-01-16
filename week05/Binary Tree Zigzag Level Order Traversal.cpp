eddfa51bc6ab03921e635d5ca560549ea53b1d3d
imShafin <izarahsha2357@gmail.com>
2023-01-17 03:42:27 +0600

    another solutions for week05

---
+++ week05/Binary Tree Zigzag Level Order Traversal.cpp
@@ -0,0 +1,26 @@
+TC: O(N*M)       ///N is total nodes, M is maximum nodes in a level
+MC: O(N)         ///N is the Total number's of Node 
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> allLevels;
+        queue<TreeNode*> nodeTracker;
+        nodeTracker.push(root);
+        bool levelStatus = true;
+        while(!nodeTracker.empty()) {
+            int len = nodeTracker.size();
+            vector<int> nodesInLevel;
+            for(int i = 0; i < len; i++) {    /// Sakib vaiya's idea
+                auto parent = nodeTracker.front();
+                nodeTracker.pop();
+                if(parent) nodesInLevel.push_back(parent->val);
+                if(parent && parent->left) nodeTracker.push(parent->left);
+                if(parent && parent->right) nodeTracker.push(parent->right);
+            }
+            if(!levelStatus) reverse(nodesInLevel.begin(), nodesInLevel.end());
+            if(nodesInLevel.size()) allLevels.push_back(nodesInLevel);
+            levelStatus = !levelStatus;
+        }
+        return allLevels;
+    }
+};
